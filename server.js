2ecee72a6b2fa7422f1f385bfdc556ce00641ee2
Ben Manning <bmanning@me.com>
2025-02-13 16:07:12 -0500

    complete exercise two

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ app.get('/greetings/:username', (req, res) => {
   res.send(`Hello there, ${req.params.username}`);
 });
 
+// exericse two
+app.get('/roll/:number', (req, res) => {
+  if (isNaN(req.params.number)) {
+    res.send('You must specify a number!');
+  } else {
+    let randomNumber = Math.ceil(Math.random() * req.params.number);
+    res.send(`You rolled a ${randomNumber}`);
+  }
+});
+
 
 app.listen(3000, () => {
   console.log('listening at port:3000');
