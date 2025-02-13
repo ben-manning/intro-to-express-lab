6c92c6665202c3d9422fb2a57412a99ce938a292
Ben Manning <bmanning@me.com>
2025-02-13 17:04:24 -0500

    complete exercise four

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const collectibles = [
   { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
 ];
 
+const shoes = [
+  { name: "Birkenstocks", price: 50, type: "sandal" },
+  { name: "Air Jordans", price: 500, type: "sneaker" },
+  { name: "Air Mahomeses", price: 501, type: "sneaker" },
+  { name: "Utility Boots", price: 20, type: "boot" },
+  { name: "Velcro Sandals", price: 15, type: "sandal" },
+  { name: "Jet Boots", price: 1000, type: "boot" },
+  { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
+
 // exericse one
 app.get('/greetings/:username', (req, res) => {
   res.send(`Hello there, ${req.params.username}`);
@@ -24,12 +35,32 @@ app.get('/roll/:number', (req, res) => {
 
 // exercise three
 app.get('/collectibles/:index', (req, res) => {
+  const index = req.params.index;
   if (!collectibles[req.params.index]) {
     res.send('This item is not yet in stock. Check back soon!');
   } else {
-    res.send(`So, you want the ${collectibles[req.params.index].name}? For ${collectibles[req.params.index].price}, it can be yours!`)
+    res.send(`So, you want the ${collectibles[index].name}? For ${collectibles[index].price}, it can be yours!`)
   }
 });
+
+// exercise four
+app.get('/shoes', (req, res) => {
+  if (req.query['min-price']) {
+    const expensiveShoes = shoes.filter(shoe => shoe.price > req.query['min-price']);
+    res.send(expensiveShoes);
+  } else if (req.query['max-price']) {
+    const cheapShoes = shoes.filter(shoe => shoe.price < req.query['max-price']);
+    res.send(cheapShoes);
+  } else if (req.query.type) {
+      const shoesOfAType = shoes.filter((shoe) => {
+        shoe.type === req.query.type
+      });
+      res.send(shoesOfAType);
+  } else {
+    res.send(shoes);
+  }
+});
+
 
 
 app.listen(3000, () => {
